df9ee2d6f3fc2103f4f53988aa591e02cc434465
Tomas Stone <tomas.stone@student-cs.fr>
2026-03-16 15:10:30 +0100

    save

---
+++ 7__robot_mission_MAS2026/agents.py
@@ -0,0 +1,37 @@
+from mesa import Agent
+
+class baseAgent(Agent):
+    def __init__(self, model):
+        super().__init__(model)
+
+        self.knowledge = {
+            "observations": [],
+            "actions": [],
+        }
+
+        self.percepts = {}
+
+    def step(self):
+        self.step_agent()
+
+    def step_agent(self):
+        self.update(self.knowledge, self.percepts, self.action)
+        action = deliberate(self.knowledge)
+        percepts = self.model.do(action)
+
+    def update(self, knowledge, percepts, action):
+        pass
+
+    def deliberate(knowledge):
+
+
+
+    
+        
+    
+
+class greenAgent(baseAgent):
+    
+class yellowAgent(baseAgent):
+
+class redAgent(baseAgent):
