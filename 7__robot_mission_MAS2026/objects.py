5ff6fe490ee5382fe631b479a5c003b1ff4c0a2e
Clara Vega <clara.vega@student-cs.fr>
2026-03-16 15:11:40 +0100

    fix objects.py

--- 7__robot_mission_MAS2026/objects.py
+++ 7__robot_mission_MAS2026/objects.py
@@ -1,30 +1,33 @@
+# Group 12
+# Members : Clara Vega, Tomas Stone, Corentin Lasne
+# Date of creation : 2025-03-16
+
 from mesa import Agent
 
 import random
 
-class RadioactivityAgent(Agent):
+class radioactivityAgent(Agent):
     """ An agent with a radioactivity level. 
     The radioactivity level is a random number between 0 and 1, depending on the zone of the grid.
     If the radioactivity level is 10, then it is the Waste Disposal Zone.
     It has no behaviour.
     """
-    def __init__(self, zone, position, model):
+    def __init__(self, model, zone):
         super().__init__(model)
-        self.position = position
         if zone == 1 :
             self.radioactivity = random.uniform(0,0.33)
         elif zone == 2 :
             self.radioactivity = random.uniform(0.33,0.66)
         elif zone == 3 :
             self.radioactivity = random.uniform(0.66,1)
+        # Waste Disposal Zone
         else :
             self.radioactivity = 10
 
-class WasteAgent(Agent):
+class wasteAgent(Agent):
     """ An agent that represents a waste. 
     It has no behaviour.
     """
-    def __init__(self, position, waste_type, model):
+    def __init__(self, model, waste_type):
         super().__init__(model)
-        self.position = position
         self.waste_type = waste_type
