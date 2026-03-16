c6ab0bbe0459702f6450337175e6a9f0de8d2ac6
Tomas Stone <tomas.stone@student-cs.fr>
2026-03-16 15:21:50 +0100

    agents

--- 7__robot_mission_MAS2026/agents.py
+++ 7__robot_mission_MAS2026/agents.py
@@ -1,13 +1,13 @@
-""" 
-Group number: 12
-Group members:
-    - Tomas Stone
-    - Clara Vega
-    - Corentin Lasne
-Date of creation : 16/03/2026
-"""
-
 from mesa import Agent
+
+actions = {
+    "move_up": 0,
+    "move_down": 1,
+    "move_right": 2,
+    "move_left": 3,
+    "pick_up": 4,
+    "drop": 5,
+}
 
 class baseAgent(Agent):
     def __init__(self, model):
@@ -24,23 +24,38 @@ class baseAgent(Agent):
         self.step_agent()
 
     def step_agent(self):
-        self.update(self.knowledge, self.percepts, self.action)
-        action = deliberate(self.knowledge)
+        self.update(self.knowledge, self.percepts)
+        action = self.deliberate(self.knowledge)
         percepts = self.model.do(action)
 
-    def update(self, knowledge, percepts, action):
+    def update(self, knowledge, percepts):
         pass
 
+    @staticmethod
     def deliberate(knowledge):
+        pass
 
-
-
-    
-        
-    
 
 class greenAgent(baseAgent):
+    def __init__(self, model):
+        super().__init__(model)
+
+        self.allowed_zones = ["z1"]
+        self.target_waste = "g"
+        self.max_capacity = 2
     
 class yellowAgent(baseAgent):
+    def __init__(self, model):
+        super().__init__(model)
+
+        self.allowed_zones = ["z1", "z2"]
+        self.target_waste = "y"
+        self.max_capacity = 2
 
 class redAgent(baseAgent):
+    def __init__(self, model):
+        super().__init__(model)
+
+        self.allowed_zones = ["z1", "z2", "z3"]
+        self.target_waste = "r"
+        self.max_capacity = 1
