d6e155106750c3d095e49a7bdbfbde77d1259d88
corentin.lasne <corentin.lasne@laposte.net>
2026-03-20 16:48:23 +0100

    Update on rules and policies of agents

--- 7__robot_mission_MAS2026/model.py
+++ 7__robot_mission_MAS2026/model.py
@@ -33,6 +33,7 @@ class Model(Model):
         self.num_waste = n_waste
         self.grid = MultiGrid(width, height, torus=False)
         self.robotAgents = []
+        self.wasteAgents = []
         
         # Define z1, z2, z3 as third area of the grid
         self.z1 = (0, 0, width//3, height)
@@ -61,35 +62,54 @@ class Model(Model):
         for i in range(self.num_waste):
             x = self.random.randrange(self.z1[0], self.z1[2])
             y = self.random.randrange(self.z1[1], self.z1[3])
-            self.grid.place_agent(wasteAgent(self, "green"), (x, y))
+            waste = wasteAgent(self, "green")
+            self.wasteAgents.append(waste)
+            self.grid.place_agent(waste, (x, y))
 
         # Create agents and place them randomly in their area
         for i in range(self.num_green_agents):
             green_agent = greenAgent(self)
             self.robotAgents.append(green_agent)
-            x = self.random.randrange(self.z1[0], self.z1[2])
-            y = self.random.randrange(self.z1[1], self.z1[3])
-            self.grid.place_agent(green_agent, (x, y))
+            pos = self.get_random_free_robot_position(self.z1)
+            self.grid.place_agent(green_agent, pos)
             
         for i in range(self.num_yellow_agents):
             yellow_agent = yellowAgent(self)
             self.robotAgents.append(yellow_agent)
-            x = self.random.randrange(self.z2[0], self.z2[2])
-            y = self.random.randrange(self.z2[1], self.z2[3])
-            self.grid.place_agent(yellow_agent, (x, y))
+            pos = self.get_random_free_robot_position(self.z2)
+            self.grid.place_agent(yellow_agent, pos)
             
         for i in range(self.num_red_agents):
             red_agent = redAgent(self)
             self.robotAgents.append(red_agent)
-            x = self.random.randrange(self.z3[0], self.z3[2])
-            y = self.random.randrange(self.z3[1], self.z3[3])
-            self.grid.place_agent(red_agent, (x, y))
+            pos = self.get_random_free_robot_position(self.z3)
+            self.grid.place_agent(red_agent, pos)
         
     def step(self):
         agent_list = list(self.robotAgents)
         self.random.shuffle(agent_list)
         for agent in agent_list:
             agent.step_agent()
+
+    def is_robot_cell_free(self, pos, moving_agent=None):
+        """Return True if no other robot agent is on this cell."""
+        cell_contents = self.grid.get_cell_list_contents([pos])
+        return not any(obj in self.robotAgents and obj is not moving_agent for obj in cell_contents)
+
+    def get_random_free_robot_position(self, zone):
+        """Pick a random position in a zone that is not occupied by another robot."""
+        x_min, y_min, x_max, y_max = zone
+        max_attempts = self.grid.width * self.grid.height
+
+        for _ in range(max_attempts):
+            pos = (
+                self.random.randrange(x_min, x_max),
+                self.random.randrange(y_min, y_max),
+            )
+            if self.is_robot_cell_free(pos):
+                return pos
+
+        raise RuntimeError("No free position available to place a robot in the selected zone")
 
     def do(self, agent, action):
         """Execute the given action for the specified agent and returns the resulting percepts."""
@@ -124,9 +144,39 @@ class Model(Model):
         if self.grid.out_of_bounds(new_pos):
             return self.get_percepts(agent)  # Return current percepts if move is invalid
 
+        # Enforce per-agent zone constraints.
+        if not self.is_position_allowed(agent, new_pos):
+            return self.get_percepts(agent)  # Return current percepts if move is invalid
+
+        # Prevent two robot agents from occupying the same cell.
+        if not self.is_robot_cell_free(new_pos, moving_agent=agent):
+            return self.get_percepts(agent)  # Return current percepts if move is invalid
+
         # Move the agent to the new position
         self.grid.move_agent(agent, new_pos)
         return self.get_percepts(agent)
+
+    def get_zone_name(self, pos):
+        """Return the zone name (z1, z2, z3) for a grid position."""
+        x, y = pos
+
+        if self.z1[0] <= x < self.z1[2] and self.z1[1] <= y < self.z1[3]:
+            return "z1"
+        if self.z2[0] <= x < self.z2[2] and self.z2[1] <= y < self.z2[3]:
+            return "z2"
+        if self.z3[0] <= x < self.z3[2] and self.z3[1] <= y < self.z3[3]:
+            return "z3"
+
+        return None
+
+    def is_position_allowed(self, agent, pos):
+        """Check whether a position belongs to one of the agent allowed zones."""
+        allowed_zones = getattr(agent, "allowed_zones", None)
+        if not allowed_zones:
+            return True
+
+        zone_name = self.get_zone_name(pos)
+        return zone_name in allowed_zones
 
     def pick_up(self, agent):
         """Pick up waste if the agent is on a cell with waste and return the resulting percepts."""
@@ -153,7 +203,8 @@ class Model(Model):
         """Get percepts for an agent (its current position and surroundings)."""
         percepts = {"position": agent.pos, "surrounding": {}}
         if agent.pos:
-            neighborhood = self.grid.get_neighborhood(agent.pos, include_center=True)
+            # Get just the 8 surrounding cells (including diagonals) and the current cell contents
+            neighborhood = self.grid.get_neighborhood(agent.pos, include_center=True, moore=True)
             for pos in neighborhood:
                 percepts["surrounding"][pos] = self.grid.get_cell_list_contents([pos])
         return percepts
